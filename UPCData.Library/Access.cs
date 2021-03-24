e20cea017b938d7513201cee9efdca2fec603cde
Usama Aziz <usamaaziz999@gmail.com>
2021-03-24 17:42:01 +0500

    Added library for Sql database project.

---
+++ UPCData.Library/Access.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace UPCData.Library
+{
+	public static class Access
+	{
+
+		public static void ExecuteProcedure(string storedProcedure, SqlParameterCollection parameters)
+		{
+			using (SqlConnection cnn = DB.SqlConnection)
+			{
+				SqlTransaction transaction = cnn.BeginTransaction("ExecuteProcedure");
+				SqlCommand cmd = new SqlCommand(storedProcedure, cnn) { Transaction = transaction };
+				try
+				{
+					cmd.ExecuteNonQuery();
+					transaction.Commit();
+				}
+				catch (Exception ex)
+				{
+					try
+					{
+						Console.WriteLine($"Commit Exception Type {ex.GetType()}");
+						Console.WriteLine($"Commit Exception Message {ex.Message}");
+						transaction.Rollback();
+					}
+					catch (Exception ex1)
+					{
+						Console.WriteLine($"Rollback Exception Type {ex1.GetType()}");
+						Console.WriteLine($"Rollback Exception Message {ex1.Message}");
+						throw ex1;
+					}
+				}
+			}
+		}
+
+	}
+}
---
+++ UPCData.Library/DB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPCData.Library
+{
+	public static class DB
+	{
+		public static async Task<SqlConnection> GetSqlConnectionAsync()
+		{
+			SqlConnection cnn = new SqlConnection(ConnectionString);
+			await cnn.OpenAsync();
+			return cnn;
+		}
+
+		public static SqlConnection SqlConnection
+		{
+			get
+			{
+				SqlConnection cnn = new SqlConnection(ConnectionString);
+				cnn.Open();
+				return cnn;
+			}
+		}
+
+		public static string ConnectionString
+		{
+			get
+			{
+				return ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
+			}
+		}
+
+	}
+}
