e2b02acce5bfb9c37f7a1d7ba4152d47568280e2
Usama Aziz <usamaaziz999@gmail.com>
2021-03-30 15:33:32 +0500

    Added Customer Entry Models.

---
+++ UPC.Library/CRMModels/CustomerEntry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPC.Library.CRMModels
+{
+	public class CustomerEntry
+	{
+		public int Id { get; set; }
+
+		public string CustomerName { get; set; }
+
+		public string Phone { get; set; }
+
+		public string Email { get; set; }
+
+		public string MarketPlace { get; set; } = "";
+
+		public string OrderNumber { get; set; } = "";
+
+		public string OrderDate { get; set; } = "";
+
+		public string Product { get; set; } = "";
+
+		public string Query { get; set; }
+
+		public string Action { get; set; }
+
+		public string WhatsappMessage { get; set; }
+
+		/// <summary>
+		/// Open/Closed
+		/// </summary>
+		public string Status { get; set; } = "Open";
+	}
+}
